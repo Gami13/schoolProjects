6455c4ec41d38c02e69a80d400d60182deadb48b
Marcin Czechowicz <gamiofficial0@gmail.com>
2024-01-03 11:15:52 +0100

    d

--- DjangoThing/dproject/app/urls.py
+++ DjangoThing/dproject/app/urls.py
@@ -1,9 +1,16 @@
+from django import urls
 from django.urls import path
 from . import views
-
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('messages/', views.messages, name='messages'),
+    path('register/', CreateView.as_view(
+        template_name='register.html',
+        form_class=UserCreationForm,
+        success_url=urls.reverse_lazy('index')
+    ), name='register')
 ]

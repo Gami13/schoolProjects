daae012dae86432ddf6e30a1669c303ddbb0b2c8
Gami <gamiofficial0@gmail.com>
2024-03-25 18:09:13 +0100

    everything except loading works

--- AvaloniaStorageSystem/AvaloniaStorageSystem/AvaloniaStorageSystem/ViewModels/MainViewModel.cs
+++ AvaloniaStorageSystem/AvaloniaStorageSystem/AvaloniaStorageSystem/ViewModels/MainViewModel.cs
@@ -1,92 +1,167 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using Avalonia.Controls;
-using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Avalonia;
+using Avalonia.Platform;
+using Avalonia.Platform.Storage;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 
 namespace AvaloniaStorageSystem.ViewModels;
 
-public  class StorageItem :INotifyPropertyChanged
+public class StorageItemSerialization(string name, bool isCritical, int count, int categoryIdx, int criticalCount)
 {
-    public string Name { get; set; } 
-    
+    public string Name = name;
+    public bool IsCritical = isCritical;
+    public int Count = count;
+    public int CategoryIdx = categoryIdx;
+    public int CriticalCount = criticalCount;
+}
+
+public class StorageItem : INotifyPropertyChanged
+{
+    public string Name { get; set; }
+    private bool _isCritical;
+
+
+    public bool IsCritical
+    {
+        get => _isCritical;
+        set
+        {
+            _isCritical = value;
+            PropertyChanged?.Invoke(nameof(IsCritical), new PropertyChangedEventArgs(nameof(IsCritical)));
+        }
+    }
+
     private int _count;
+
+
     public int Count
     {
         get => _count;
-        set { _count = value; PropertyChanged?.Invoke(nameof(Count), new PropertyChangedEventArgs(nameof(Count))); }
+        set
+        {
+            _count = value < 0 ? 0 : value;
+            PropertyChanged?.Invoke(nameof(Count), new PropertyChangedEventArgs(nameof(Count)));
+        }
     }
 
-    public event PropertyChangedEventHandler PropertyChanged;
-    public int ChangeStep { get; set; } = 1;
-    public void Add(int count) => Count += count;
-    public void Remove(int count) => Count -= count;
-    
-    public StorageItem(string name, int count)
+    public int CategoryIdx { get; set; }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public int CriticalCount { get; set; } = 0;
+
+    public void Add()
     {
-        this.Name = name;
-        this.Count = count;
+        Count += 1;
+        IsCritical = Count <= CriticalCount;
     }
-    
-    
+
+    public void Remove()
+    {
+        Count -= 1;
+        IsCritical = Count <= CriticalCount;
+    }
+
+    public StorageItem(string name, int categoryIdx, int count, Category category)
+    {
+        Name = name;
+        Count = count;
+        CategoryIdx = categoryIdx;
+        CriticalCount = category.CriticalCount;
+    }
+
+    public StorageItem()
+    {
+    }
+}
+
+public class Category(string name, int criticalCount)
+{
+    public string Name { get; set; } = name;
+    public int CriticalCount { get; set; } = criticalCount;
 }
 
 public class MainViewModel : ViewModelBase
 {
+    public ObservableCollection<Category> Categories { get; set; }
 
-    public ObservableCollection<StorageItem> Items { get; }
+    public ObservableCollection<StorageItem> Items { get; set; }
+
 
     public MainViewModel()
     {
-        var items = new List<StorageItem>
-        {
-            new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),
-            new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),        new StorageItem("Coca-Cola 0.5L", 10),
-            new StorageItem("Pepsi 0.5L", 5),
-            new StorageItem("Fanta 0.5L", 7),
-        };
-        Items = new ObservableCollection<StorageItem>(items);
-       
+        Categories =
+        [
+            new Category("Drinks", 5),
+            new Category("Food", 3),
+            new Category("Other", 0)
+        ];
+        Items =
+        [
+            new StorageItem("Coca-Cola 0.5L", 0, 10, Categories[0]),
+            new StorageItem("Pepsi 0.5L", 0, 5, Categories[0]),
+            new StorageItem("Fanta 0.5L", 0, 7, Categories[0]),
+        ];
+        Load();
+        Debug.WriteLine("HELLO");
     }
 
-    private void OnPropertyChanged(string itemsName)
+    public void Save()
     {
-        throw new System.NotImplementedException();
+        var filePath =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "data.json");
+        MessageBoxManager.GetMessageBoxStandard("Saved", "Data saved to " + filePath, ButtonEnum.Ok).ShowAsync();
+        Debug.WriteLine("Saving to: " + filePath);
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            writer.WriteLine(JsonSerializer.Serialize(Items));
+
+            writer.WriteLine(JsonSerializer.Serialize(Categories));
+        }
+    }
+
+    private void Load()
+    {
+        Debug.WriteLine("Loading");
+        try
+        {
+            var filePath =
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    "data.json");
+            Debug.WriteLine("Loading from: " + filePath);
+            if (!File.Exists(filePath)) return;
+            using var reader = new StreamReader(filePath);
+
+            var items = JsonSerializer.Deserialize<ObservableCollection<StorageItemSerialization>>(reader.ReadLine());
+           
+            Categories = JsonSerializer.Deserialize<ObservableCollection<Category>>(reader.ReadLine());
+            var newItems = new ObservableCollection<StorageItem>();
+            foreach (var item in items)
+            {
+                Debug.WriteLine("ITEM: " + item.Name);
+                var newItem = new StorageItem
+                {
+                    Name = item.Name,
+                    Count = item.Count,
+                    CategoryIdx = item.CategoryIdx,
+                    IsCritical = item.IsCritical,
+                    CriticalCount = item.CriticalCount
+                };
+                newItems.Add(newItem);
+            }
+
+            Items = newItems;
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("ERROR " + e.Message);
+        }
     }
 }
--- AvaloniaStorageSystem/AvaloniaStorageSystem/AvaloniaStorageSystem/Views/MainView.axaml.cs
+++ AvaloniaStorageSystem/AvaloniaStorageSystem/AvaloniaStorageSystem/Views/MainView.axaml.cs
@@ -1,4 +1,9 @@
+using System.Diagnostics;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Logging;
+using AvaloniaStorageSystem.ViewModels;
 
 namespace AvaloniaStorageSystem.Views;
 
@@ -7,5 +12,48 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+
+
+    private void SelectingItemsControl_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        Debug.WriteLine("SELECTION CHANGED");
+        var vm = this.DataContext as MainViewModel;
+        //GET WHICH ITEM IS SELECTED
+        var Item = (sender as ComboBox).DataContext as StorageItem;
+        Debug.WriteLine("Selected Item: " + Item.Name);
+        //GET WHICH CATEGORY IS SELECTED
+        var Category = vm.Categories[Item.CategoryIdx];
+        Debug.WriteLine("Selected Category: " + Category.Name);
+        Item.CriticalCount = Category.CriticalCount;
+        Item.IsCritical = Item.Count <= Item.CriticalCount;
+    }
+
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var vm = this.DataContext as MainViewModel;
+        var Name = addName.Text;
+        var Category = vm.Categories[addCategory.SelectedIndex];
+        var Count = int.Parse(addCount.Text);
+        vm.Items.Add(new StorageItem(Name, addCategory.SelectedIndex, Count, Category));
+        addName.Text = "";
+        addCount.Text = "";
+    }
+
+    private void CategoryButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var vm = this.DataContext as MainViewModel;
+        var Name = newCategory.Text;
+        var CriticalCount = int.Parse(newCritical.Text);
+        vm.Categories.Add(new Category(Name, CriticalCount));
+        newCategory.Text = "";
+        newCritical.Text = "";
+    }
+
+    private void Save(object? sender, RoutedEventArgs e)
+    {
+        var vm = this.DataContext as MainViewModel;
+        vm.Save();
     }
 }

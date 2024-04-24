d168a15df63086eb52292c8095a005d72ef81ed8
Marcin Czechowicz <gamiofficial0@gmail.com>
2024-04-24 10:16:59 +0200

    k

---
+++ EgzaminDesktop/EgzaminDesktop/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EgzaminDesktop;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private string number = "";
+    private string name = "";
+    private string lastName = "";
+    private string eyeColor = "niebieskie";
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (number.Length == 0 || name.Length == 0 || lastName.Length == 0)
+        {
+            MessageBox.Show("Wprowadź dane");
+
+            return;
+        }
+
+        MessageBox.Show($"{name} {lastName} kolor oczu {eyeColor}");
+    }
+
+    private void BlueEyes(object sender, RoutedEventArgs e)
+    {
+        eyeColor = "niebieskie";
+    }
+
+    private void GreenEyes(object sender, RoutedEventArgs e)
+    {
+        eyeColor = "zielone";
+    }
+
+    private void BeerEyes(object sender, RoutedEventArgs e)
+    {
+        eyeColor = "piwne";
+    }
+
+    private void NumberInput(object sender, TextChangedEventArgs textChangedEventArgs)
+    {
+        number = ((TextBox)sender).Text;
+        if (number == "111" || number == "222" || number == "333")
+        {
+            PersonImage.Source = new BitmapImage(new Uri($"pack://application:,,,/EgzaminDesktop;component/Resources/{number}-zdjecie.png"));
+            FingerprintImage.Source = new BitmapImage(new Uri($"pack://application:,,,/EgzaminDesktop;component/Resources/{number}-odcisk.png"));
+
+        }
+    }
+
+    private void NameInput(object sender, TextChangedEventArgs textChangedEventArgs)
+    {
+        name = ((TextBox)sender).Text;
+    }
+
+    private void LastNameInput(object sender, TextChangedEventArgs textChangedEventArgs)
+    {
+        lastName = ((TextBox)sender).Text;
+    }
+}
---
+++ EgzaminKonsolowa/Egzamin/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics;
+int[] weights = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
+string pesel = "05220406236";
+
+//**********************************************
+//nazwa funkcji: handleInput
+//opis funkcji: pobiera od użytkownika numer PESEL
+//parametry: brak
+//zwracany typ i opis: string - jest to numer PESEL
+//autor: 05220406236
+//***********************************************
+
+string handleInput()
+{
+    string input = Console.ReadLine();
+    if (input.Length != 11)
+    {
+        Console.WriteLine("Niepoprawna długość numeru PESEL");
+        Console.WriteLine(
+            "Wciśnij 1 aby wpisać ponownie, wciśnij 2 aby użyc wartości domyślnej, wciśnij 3 aby zakończyć");
+        string choice = Console.ReadLine();
+        if (choice == "1")
+        {
+            Console.WriteLine("Proszę wprowadzić numer pesel: :3");
+            input = Console.ReadLine();
+        }
+        else if (choice == "2")
+        {
+            Console.WriteLine("Używam wartości domyślnej");
+            input = pesel;
+        }
+        else if (choice == "3")
+        {
+            return "0";
+        }
+    }
+
+    return input;
+}
+//**********************************************
+//nazwa funkcji: checkSex
+//opis funkcji: Sprawdza płeć na podstawie numeru PESEL
+//parametry: pesel - numer PESEL użytkownika
+//zwracany typ i opis: string - jest to płeć użytkownika, K - kobieta, M - mężczyzna
+//autor: 05220406236
+//***********************************************
+string checkSex(string pesel)
+{
+    if (pesel != null)
+    {
+    }
+        return Int32.Parse(pesel[9].ToString()) % 2 == 0 ? "K" : "M";
+}
+//**********************************************
+//nazwa funkcji: checkValidity
+//opis funkcji: sprawdza poprawność numeru PESEL na podstawie algorytmu wagowego
+//parametry: pesel - numer PESEL użytkownika
+//zwracany typ i opis: boolean - true jeśli numer PESEL jest poprawny, false w przeciwnym wypadku
+//autor: 05220406236
+//***********************************************
+bool checkValidity(string pesel)
+{
+    int S = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        S += Int32.Parse(pesel[i].ToString()) * weights[i];
+    }
+    int M = S % 10;
+    int R = M==0 ? 0 : 10 - M;
+    return R == Int32.Parse(pesel[10].ToString());
+}
+
+Console.WriteLine("Proszę wprowadzić numer pesel: :3");
+string input = handleInput();
+//sprawdzanie czy użytkownik wprowadził numer PESEL
+if (input.Length != 11)
+{
+    Console.WriteLine("Niepoprawna długość numeru PESEL, zakończono program");
+    return;
+}
+pesel = input;
+string sex = checkSex(pesel);
+bool valid = checkValidity(pesel);
+Console.WriteLine("Podany numer PESEL: " + pesel);
+Console.WriteLine(sex=="K" ? "Kobieta" : "Mężczyzna");
+Console.WriteLine("Numer PESEL jest " + (valid ? "poprawny" : "niepoprawny"));
